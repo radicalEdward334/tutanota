e52b97c1f84ffbe5c1745af4bc46d330e54719bf
jom <jom@tutao.de>
2020-11-09 14:51:06 +0100

    WIP handle logins/signups at redemption.

--- src/api/main/WorkerClient.js
+++ src/api/main/WorkerClient.js
@@ -592,6 +592,11 @@ export class WorkerClient implements EntityRestInterface {
 	}
 
 	//TODO redeemGiftCard(userId, giftCardId)
+	redeemGiftCard(userId: Id, giftCardId: Id): Promise<boolean> {
+		return this._queue.postMessage(new Request("redeemGiftCard", [userId, giftCardId]))
+		           .tap(success => console.log(success ? "Successfully" : "Unsuccessfully", "redeemed gift card:", giftCardId, "for user", userId))
+
+	}
 }
 
 export const worker: WorkerClient = new WorkerClient()
